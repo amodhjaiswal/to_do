3c04db5bdfc414e13871a7fb9c51230f6f86f7b1
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-09-21 20:10:35 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /home/ubuntu
-npm install
+echo "Changing directory to /home/ubuntu"
+cd /home/ubuntu || { echo "Failed to change directory"; exit 1; }
+
+# Check if package.json exists
+if [[ ! -f package.json ]]; then
+    echo "Error: package.json not found!"
+    exit 1
+else
+    echo "Found package.json, running npm start"
+fi
+
 npm start
-
-
