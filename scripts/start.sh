eac517fa945c2a5520f0ec57ec1e4a8dd6710aed
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-09-21 20:15:33 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-echo "Changing directory to /home/ubuntu"
-cd /home/ubuntu || { echo "Failed to change directory"; exit 1; }
-
-# Check if package.json exists
-if [[ ! -f package.json ]]; then
-    echo "Error: package.json not found!"
-    exit 1
-else
-    echo "Found package.json, running npm start"
-fi
-
+cd /home/ubuntu
 npm start
