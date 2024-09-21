5a5b0bc061b574d146d3abee94aee94a0f29b76e
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-09-21 19:39:45 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-# Check if the directory exists, create it if it doesn't
-if [ ! -d "/home/ubuntu/to-do" ]; then
-    mkdir /home/ubuntu/to-do
-fi
-
-# Navigate to the directory
+mkdir /home/ubuntu/to-do
 cd /home/ubuntu/to-do
 
 
