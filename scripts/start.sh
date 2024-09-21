50dbf004d2a530ef4a59af6fe4f66a3854316b16
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-09-21 19:51:30 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-
-# Define the directory
-TARGET_DIR="/home/ubuntu/to-do"
-
-# Check if the directory exists, create it if it doesn't
-if [ ! -d "$TARGET_DIR" ]; then
-    mkdir -p "$TARGET_DIR"
-fi
-
-# Navigate to the directory
-cd "$TARGET_DIR" || { echo "Failed to change directory to $TARGET_DIR"; exit 1; }
-
-
-
-# Run npm install or any other npm command
-npm install
+cd /home/ubuntu/
 npm start
 
 
